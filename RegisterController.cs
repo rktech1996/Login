ee17e35bf07deec8e06c97a9cd993d1ae14e1d90
rktech1996 <43445314+rktech1996@users.noreply.github.com>
2020-11-19 22:28:34 +0530

    Add files via upload

---
+++ RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
